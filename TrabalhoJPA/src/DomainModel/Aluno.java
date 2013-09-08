566d52b3338aaf98956ea6e75d8f3c7c3dbbf53f
Iara <Iara@validation.sls.microsoft.com>
2013-09-08 00:18:51 -0300

    Trabalho JPA

---
+++ TrabalhoJPA/src/DomainModel/Aluno.java
@@ -0,0 +1,136 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package DomainModel;
+
+import java.io.Serializable;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+/**
+ *
+ * @author Iara
+ */
+@Entity
+public class Aluno implements Serializable{
+    @Id
+    @GeneratedValue(strategy= GenerationType.AUTO)
+    private int idAluno;
+    @Column(nullable=false)
+    private String nome;
+    @Column(nullable=false)
+    private String cpf;
+    @Column(nullable=false)
+    private String rg;
+    @Column(nullable=false)
+    private String telefone;
+    @Column(nullable=false)
+    private String email;
+    @Column(nullable=false)
+    private double media;
+    @Column(nullable=false)
+    private String nomeresp;
+    @Column(nullable=false)
+    private String telefoneresp;
+    private int idcurso;
+    
+    public Aluno(){
+        idAluno =0;
+        nome = "";
+        cpf = "";
+        rg = "";
+        telefone = "";
+        email = "";
+        media = 0;
+        nomeresp = "";
+        telefoneresp = "";
+    }
+
+    public int getIdAluno() {
+        return idAluno;
+    }
+
+    public void setIdAluno(int idAluno) {
+        this.idAluno = idAluno;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getCpf() {
+        return cpf;
+    }
+
+    public void setCpf(String cpf) {
+        this.cpf = cpf;
+    }
+
+    public String getRg() {
+        return rg;
+    }
+
+    public void setRg(String rg) {
+        this.rg = rg;
+    }
+
+
+    public double getMedia() {
+        return media;
+    }
+
+    public void setMedia(double media) {
+        this.media = media;
+    }
+
+    public String getNomeresp() {
+        return nomeresp;
+    }
+
+    public void setNomeresp(String nomeresp) {
+        this.nomeresp = nomeresp;
+    }
+
+    public String getTelefoneresp() {
+        return telefoneresp;
+    }
+
+    public void setTelefoneresp(String telefoneresp) {
+        this.telefoneresp = telefoneresp;
+    }
+
+    public int getIdcurso() {
+        return idcurso;
+    }
+
+    public void setIdcurso(int idcurso) {
+        this.idcurso = idcurso;
+    }
+
+    public String getTelefone() {
+        return telefone;
+    }
+
+    public void setTelefone(String telefone) {
+        this.telefone = telefone;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+    
+    
+    
+}
