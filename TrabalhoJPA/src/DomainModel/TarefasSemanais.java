c41a26ded23508b2d86c5c7f9e9846a4044e1ccc
Iara <Iara@validation.sls.microsoft.com>
2013-09-08 00:41:12 -0300

    Trabalho JPA

---
+++ TrabalhoJPA/src/DomainModel/TarefasSemanais.java
@@ -0,0 +1,47 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package DomainModel;
+
+import java.io.Serializable;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+/**
+ *
+ * @author Iara
+ */
+@Entity
+public class TarefasSemanais implements Serializable {
+    @Id
+    @GeneratedValue(strategy= GenerationType.AUTO)
+    private int idTarefas;
+    @Column(nullable=false)
+    private String descricao;
+    
+    public TarefasSemanais(){
+        this.idTarefas = 0;
+        this.descricao = "";
+    }
+
+    public int getIdTarefas() {
+        return idTarefas;
+    }
+
+    public void setIdTarefas(int idTarefas) {
+        this.idTarefas = idTarefas;
+    }
+
+    public String getDescricao() {
+        return descricao;
+    }
+
+    public void setDescricao(String descricao) {
+        this.descricao = descricao;
+    }
+    
+}
