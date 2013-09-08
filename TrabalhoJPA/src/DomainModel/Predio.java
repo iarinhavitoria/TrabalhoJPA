27803cc627768a6b198d1bdc96d2348f8369bcdb
Iara <Iara@validation.sls.microsoft.com>
2013-09-08 10:50:35 -0300

    Trabalho JPA

---
+++ TrabalhoJPA/src/DomainModel/Predio.java
@@ -0,0 +1,66 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package DomainModel;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+/**
+ *
+ * @author Iara
+ */
+@Entity
+public class Predio {
+    @Id
+    @GeneratedValue(strategy= GenerationType.AUTO)
+    private int idPredio;
+    @Column(name="Nome",length=200)
+    private String nome;
+    @Column(nullable=false)
+    private int codfuncionario;
+    private int codcampus;
+    
+    public Predio(int id, String nome){
+        this.idPredio = id;
+        this.nome = nome;
+    }
+
+    public int getIdPredio() {
+        return idPredio;
+    }
+
+    public void setIdPredio(int idPredio) {
+        this.idPredio = idPredio;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public int getCodfuncionario() {
+        return codfuncionario;
+    }
+
+    public void setCodfuncionario(int codfuncionario) {
+        this.codfuncionario = codfuncionario;
+    }
+
+    public int getCodcampus() {
+        return codcampus;
+    }
+
+    public void setCodcampus(int codcampus) {
+        this.codcampus = codcampus;
+    }
+    
+    
+}
